cfd62efa6e64d34245f4bc0d113c2da3891ab1f3
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-01-11 01:39:45 +0500

    memo callback

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { useState } from "react";
-const Child = ({ changeState }) => {
-  const [text, setText] = useState('');
-  return (
-    <>
-      <input type="text" onChange={(e) => setText(e.target.value)} />
-      <button onClick={changeState(text)}>Change Parent State</button>
-    </>
-  );
-};
-const App = () => {
-  const [name, setName] = useState('');
-  const changeState = (value) => {
-    setName(value);
-  };
-  return (
-    <>
-      <h1>Parent state -- {name}</h1>
-      <Child changeState={changeState} />
-    </>
+const { useState, useMemo, useCallback } = require("react");
 
-  );
-};
+function ChildComponent() {
+  console.log("child component rendered");
 
-export default App;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+function App() {
+  const [num, setNum] = useState(0);
+  const handleUpdateNum =useCallback(()=>{
+       console.log("rnnnnninnnngggg ");
+  },[])
+
+  const getChildComp = useMemo(() => <ChildComponent handleUpdateNum={handleUpdateNum}/>, [handleUpdateNum]);
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      {getChildComp}
+      <button onClick={() => setNum(num + 1)}> Addition </button>
+    </div>
+  );
+}
+export default App
--- src/Refs/Child.js
+++ src/Refs/Child.js
@@ -1,10 +1,12 @@
 import React, { forwardRef } from "react";
+import { useState } from "react";
 
 const Child = (props, ref) => {
-  console.log(ref);
+  const [first, setfirst] = useState(null);
   return (
     <>
-      <input type="text" ref={ref} />
+      <input type="text" ref={ref} onChange={(e) => setfirst(e.target.value)} />
+      <button onClick={() => props.updateInput(first)}>Update input box</button>
     </>
   );
 };
